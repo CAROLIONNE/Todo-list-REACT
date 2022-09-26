bd15d87eaac55695b080cf111aa45852029dd989
caroline soc <soc.caroline1991@gmail.com>
2022-09-26 11:04:13 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import "./App.css";
+import { useState } from "react";
+import "../node_modules/bulma/css/bulma.min.css";
+import Header from "./Composants/Header/Header";
+import Card from "./Composants/Card/Card";
+
+function App() {
+  const [monState, setMonState] = useState([
+    { tache: "Promener le chien", txt: "Une fois a 8h, une fois à 18h" },
+    { tache: "Préparer le repas", txt: "Acheter du poisson + riz" },
+    { tache: "Créer une app web", txt: "Apprendre REACT" },
+  ]);
+
+  const [tache, setTache] = useState();
+  const [txt, setTxt] = useState();
+
+  function creationCarte(e) {
+    e.preventDefault();
+    const nvTab = [...monState, { tache: tache, txt: txt }];
+    setMonState(nvTab);
+    setTache('');
+    setTxt('');
+  }
+
+  function supprCarte(index) {
+    const tabNettoyage = [...monState];
+    setMonState(
+      tabNettoyage.filter(
+        (item) =>
+          tabNettoyage.indexOf(item) !==
+          tabNettoyage.indexOf(tabNettoyage[index])
+      )
+    );
+  }
+  return (
+    <div className="App">
+      <Header />
+
+      <div className="container px-3">
+        <h2 className="title mt-5">Rentrez vos tâches à faire</h2>
+        <form onSubmit={creationCarte}>
+          <div className="field">
+            <div className="control">
+              <label htmlFor="tache" className="label"></label>
+              <input
+              value ={tache}
+                className="input"
+                type="text"
+                id="tache"
+                placeholder="Une tâche à faire"
+                onChange={(e) => setTache(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <div className="control">
+              <label htmlFor="txt" className="label">
+                Contenu de la tâche
+              </label>
+              <textarea
+              value ={txt}
+                className="textarea"
+                type="text"
+                id="txt"
+                placeholder="Une tâche à faire"
+                onChange={(e) => setTxt(e.target.value)}
+              ></textarea>
+            </div>
+          </div>
+
+          <div className="control">
+            <button type="submit" className="button is-link">
+              Créez une tâche
+            </button>
+          </div>
+        </form>
+        {monState.map((todo, index) => {
+          return (
+            <Card
+              key={index}
+              index={index}
+              tache={todo.tache}
+              txt={todo.txt}
+              supprFunc={supprCarte}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
